c39f604f8a0d37acbee6ed60aea442e3df4949eb
MiguelRemedios <1919716@brunel.ac.uk>
2021-05-30 16:52:38 +0100

    Input objects and TODO array with respective functions on Form.js

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 //Components import
 import Form from "./components/Form";
 import List from "./components/List";
 
 function App() {
+  //states and set functions for the typed input as well as for the todos array
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <header />
       <header>
         <h1>TODO LIST</h1>
       </header>
-      <Form />
-      <List />
+      <Form
+        input={input}
+        setInput={setInput}
+        todos={todos}
+        setTodos={setTodos}
+      />
+      <List input={input} />
     </div>
   );
 }
--- todoapp/src/components/Form.js
+++ todoapp/src/components/Form.js
@@ -1,15 +1,37 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function Form() {
+function Form(props) {
+  const { input, setInput, todos, setTodos } = props;
+  const inputHandler = (e) => {
+    const input = e.target.value;
+    setInput(input);
+  };
+
+  const submitTODOHandler = (e) => {
+    e.preventDefault();
+
+    const UUID = uuidv4();
+    setTodos([...todos, { id: UUID, text: input, completed: false }]);
+    setInput("");
+  };
+
   return (
     <form>
       <input
+        onChange={inputHandler}
+        value={input}
         id="input"
         type="text"
         placeholder="Add a task to your list"
         className="todo-input"
       />
-      <button id="btn" className="todo-button" type="submit">
+      <button
+        id="btn"
+        className="todo-button"
+        type="submit"
+        onClick={submitTODOHandler}
+      >
         <i className="fa fa-plus-circle"></i>
       </button>
     </form>
