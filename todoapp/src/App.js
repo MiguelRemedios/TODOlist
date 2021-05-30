448503bafd76f5f19c7ebfea274b1efd91eb924d
MiguelRemedios <1919716@brunel.ac.uk>
2021-05-30 15:50:02 +0100

    Form structure update and css refactor

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 //Components import
-import Form from './components/Form';
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
+      <header />
       <header>
-        <h1>TODO App</h1>
+        <h1>TODO LIST</h1>
       </header>
       <Form />
     </div>
